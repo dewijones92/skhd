9fcb99ab16d432e376716fa4c8c0f0444331e3f6
Dewi Jones <dewijones92@gmail.com>
2019-02-17 22:44:37 +0000

    Create symlink

--- go.sh
+++ go.sh
@@ -2,4 +2,5 @@ make install      # release version
 cp examples/skhdrc ~/.skhdrc
 cp bin/skhd /usr/local/bin/
 cp examples/com.koekeishiya.skhd.plist ~/Library/LaunchAgents
+ln -s examples/skhdrc ~/.skhdrc
 launchctl load -F -w ~/Library/LaunchAgents/com.koekeishiya.skhd.plist;
