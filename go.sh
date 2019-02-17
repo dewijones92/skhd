2f2ea0f01bea5d383e5844e5af4906a86f04b8a6
Dewi Jones <dewijones92@gmail.com>
2019-02-17 22:55:19 +0000

    Fix script

--- go.sh
+++ go.sh
@@ -1,6 +1,5 @@
 make install      # release version
-cp examples/skhdrc ~/.skhdrc
 cp bin/skhd /usr/local/bin/
 cp examples/com.koekeishiya.skhd.plist ~/Library/LaunchAgents
-ln -s examples/skhdrc ~/.skhdrc
+ln -s -f custom_config/skhdrc ~/.skhdrc
 launchctl load -F -w ~/Library/LaunchAgents/com.koekeishiya.skhd.plist;
